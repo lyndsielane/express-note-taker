ed5ecf6fb16afa2d4e772d335afd25c13656d40e
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-15 17:33:16 -0400

    added unique ids to notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,12 @@
 const notes = require('../db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(notes))
     app.post('/api/notes', (req, res) => {
+        const noteId = uuidv4();
+        req.body.id = noteId;
         notes.push(req.body);
         fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
             if (err) {
