664bce188c2187f8ae5cbc186638c5aeba941754
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-15 17:02:45 -0400

    fixed file path typo

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes.htmlRoutes')(app);
+//require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
