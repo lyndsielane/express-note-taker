4cbbe980a7dccf9a085dd6b2b5e0dee7f61cd0cd
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-15 16:26:05 -0400

    server connected

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const http = require('http');
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.get('/', function (req, res) {
+    res.send('hello world')
+});
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
